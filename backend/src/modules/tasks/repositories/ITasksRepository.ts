93ecd1ca6ff8c212f6109449e3a849c97c263928
GBTelles1 <gbertelles@gmail.com>
2024-11-06 09:21:04 -0300

    update: tasks repository
    Remove getByTitle method (will not be used)
    Add ascending order by createdAt to getAll method

--- backend/src/modules/tasks/repositories/ITasksRepository.ts
+++ backend/src/modules/tasks/repositories/ITasksRepository.ts
@@ -4,7 +4,6 @@ import { ICreateTaskDTO } from "../dtos";
 export interface ITasksRepository {
   create({ title, description }: ICreateTaskDTO): Promise<Task>;
   getAll(): Promise<Task[]>;
-  getByTitle(title: string): Promise<Task[]>;
   getById(taskId: string): Promise<Task>;
   update(taskToUpdate: Task): Promise<Task>;
   delete(taskId: string): Promise<Task>;
